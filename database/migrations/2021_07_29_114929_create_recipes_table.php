e9fff3de426ca218870ad255255c0bed34353ff7
160197415 <160197415@aston.ac.uk>
2021-07-29 13:06:48 +0100

    Migration creation

--- database/migrations/2021_07_29_114929_create_recipes_table.php
+++ database/migrations/2021_07_29_114929_create_recipes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RecipeTable extends Migration
+class CreateRecipesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class RecipeTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class RecipeTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('recipes');
     }
 }
