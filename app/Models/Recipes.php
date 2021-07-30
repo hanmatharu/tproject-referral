26ce1189173559f3fd88fb36afd58fed2d53f267
160197415 <160197415@aston.ac.uk>
2021-07-30 13:32:05 +0100

    model creation

--- app/Models/Recipes.php
+++ app/Models/Recipes.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipes extends Model
 {
-    use HasFactory;
+     use HasFactory;
+
+
+    //restricts columns from modifying 
+    protected $guarded = [];
+
+    // returns the instance of the user who is author of that post
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'author_id');
+    }
+
+   
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,20 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements AuthenticatableContract, CanResetPasswordContract
 {
     use HasFactory, Notifiable;
+
+
+     /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +29,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password'
     ];
 
     /**
@@ -29,8 +39,33 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
-    ];
+        'remember_token'
+    ]; 
+    /**
+     * User has many posts
+     */
+    public function recipes()
+    {
+        return $this->hasMany('App\Models\Recipes', 'author_id');
+    }
+
+    public function can_post()
+    {
+      $role = $this->role;
+      if ($role == 'author' || $role == 'admin') {
+        return true;
+      }
+      return false;
+    }
+
+    public function is_admin()
+    {
+      $role = $this->role;
+      if ($role == 'admin') {
+        return true;
+      }
+      return false;
+    }
 
     /**
      * The attributes that should be cast to native types.
