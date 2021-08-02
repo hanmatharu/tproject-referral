4510d8fd48d1e15ed1985f9178ef06f6545deabc
hanmatharu <200234474@aston.ac.uk>
2021-08-02 20:06:03 +0100

    Changes final

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,11 +5,13 @@
 @section('content')
     <div class="container">
         <div class="row">
+        <div class="container">
             <div class="col-12 pt-2">
                  <div class="row">
                     <div class="col-8">
                         <h1 class="display-one">Recipes Blog!</h1>
                         <p>Enjoy reading our recipes. Click on a Recipe to read!</p>
+                        <br>
                     </div>
                 </div>
                 @forelse($posts as $post)
@@ -19,7 +21,9 @@
                 @empty
                     <p class="text-warning">No recipe Posts available</p>
                 @endforelse
+                <br>
             </div>
+        </div>
         </div>
     </div>
 @endsection
